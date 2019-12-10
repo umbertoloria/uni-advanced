534ad350240a3b18a67b30b3808f3dfde9be058f
Marioegidio <39814508+Marioegidio@users.noreply.github.com>
2019-12-10 19:20:09 +0100

    Concorrenza JoinableQueue

---
+++ esercizi/concorrenza/esame 12-7/es4_queue.py
@@ -0,0 +1,41 @@
+import multiprocessing
+
+
+def conta(str, files, concurrency):
+	jobs = multiprocessing.JoinableQueue()
+	create_processes(jobs, str, files, concurrency)
+	add_jobs(str, files, jobs)
+	jobs.join()
+
+
+def create_processes(jobs, str, files, concurrency):
+	for _ in range(concurrency):
+		process = multiprocessing.Process(target=worker, args=(jobs, str, files))
+		process.daemon = True
+		process.start()
+
+
+def add_jobs(str, files, jobs):
+	for i, file in enumerate(files):
+		f = open(file, "r")
+		strsRead = f.read()
+		f.close()
+		jobs.put((i, strsRead))
+
+
+def worker(jobs,str, files):
+	while True:
+		i,strRead = jobs.get()
+		count=strRead.count(str)
+		print("La parola {} è stata trovata nel file {} {} volte".format(str, files[i], count))
+		jobs.task_done()
+
+
+
+def main():
+	files = ["f1.txt", "f2.txt", "f3.txt", "f8.txt"]
+	conta("ciao", files, 4)
+
+
+if __name__ == "__main__":
+	main()
